93313e0104e50639435d9cac75a783c522c589eb
Mridul Kumar <mridulkumar1998@gmail.com>
2024-08-24 18:32:50 +0530

    show model in admin portal

--- blog/service/admin.py
+++ blog/service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from service.models import Service
 
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service_icon', 'service_title', 'service_desc')
 # Register your models here.
+
+admin.site.register(Service, ServiceAdmin)
