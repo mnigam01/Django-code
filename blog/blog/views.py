ddff02dee6b4e0ee88360b08c04af1bf33aab8ff
Mridul Kumar <mridulkumar1998@gmail.com>
2024-08-24 18:46:49 +0530

    fetch data from database

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from service.models import Service
 
 def say_hello(request):
+    service_data = Service.objects.all()
+    print(service_data)
     return HttpResponse("Hello World!")
 
 def course_enroller(request, course_id):
--- blog/service/models.py
+++ blog/service/models.py
@@ -6,5 +6,8 @@ class Service(models.Model):
     service_title = models.CharField(max_length=50)
     service_desc = models.TextField()
     
+    def __str__(self) -> str:
+        return f"{self.service_title}\n {self.service_desc[:30]}\n"
+    
 
     
